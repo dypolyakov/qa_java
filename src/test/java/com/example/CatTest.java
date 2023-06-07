81cccb9268f85e3298db16f24c04436b592169fd
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-07 20:02:17 +0500

    Added tests for the Cat class

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,20 +1,42 @@
 package com.example;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
-    @Spy
-    Cat cat = new Cat(new Feline());
+    @Mock
+    private Feline feline;
+
+    private Cat cat;
+
+
+    @Before
+    public void setup() {
+        cat = new Cat(feline);
+    }
 
     @Test
     public void getSound() {
         Assert.assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void getFood() throws Exception {
+        cat.getFood();
+        List<String> food = List.of("Royal Canin", "Pro Plan", "Grandorf");
+        Mockito.when(feline.eatMeat()).thenReturn(food);
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+        List<String> actual = cat.getFood();
+        Assert.assertEquals(food, actual);
+
     }
 }
