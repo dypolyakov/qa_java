fc6a7250300fe43ebddea6d22deda6beee62a00e
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-12 14:34:12 +0500

    A new exception test has been added + code optimization

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,12 +1,9 @@
 package com.example;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
@@ -32,17 +29,25 @@ public class LionTest {
     @Test
     public void doesHaveManeMaleReturnsTrue() throws Exception {
         lion = new Lion(feline, "Самец");
-        boolean expected = true;
-        boolean actual = lion.hasMane;
-        assertEquals(expected, actual);
+        assertTrue(lion.doesHaveMane());
     }
 
     @Test
     public void doesHaveManeFemaleReturnsFalse() throws Exception {
         lion = new Lion(feline, "Самка");
-        boolean expected = false;
-        boolean actual = lion.hasMane;
-        assertEquals(expected, actual);
+        assertFalse(lion.doesHaveMane());
+    }
+
+    @Test
+    public void doesHaveManeUnknownSexThrowsException() throws Exception {
+        try {
+            lion = new Lion(feline, "sex");
+            fail("Ожидается Exception");
+        } catch (Exception exception) {
+            String expected = "Используйте допустимые значения пола животного - самей или самка";
+            String actual = exception.getMessage();
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
